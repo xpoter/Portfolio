d9c295e39c6027aa26e47e36c0392fa0f7d53f78
= <=>
2019-10-24 01:36:52 +0100

    added input verification for message and name not to be blank

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -6,5 +6,10 @@
 
     $subject    = 'Personal from '.$name;
     $message    = nl2br($message);
-
+if(isset($message) && !empty($message) && isset($name) && !empty($name)){
+    //check whether message and sender name is set before being sent
     mail('aminukano585@gmail.com', $subject, $message);
+}else{
+    echo "Message cannot be blank";
+}
+    
